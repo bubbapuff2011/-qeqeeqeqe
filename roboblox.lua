5034e07886997f23f9f67120f2afd5662fc888cb
bubbapuff2011 <98616123+bubbapuff2011@users.noreply.github.com>
2024-03-21 18:24:20 +0000

    Add files via upload

--- roboblox.lua
+++ roboblox.lua
@@ -57,17 +57,28 @@ VisualTab:AddButton({
 	Callback = function()
         for _, player in pairs(Players:GetPlayers()) do
             if player ~= Players.LocalPlayer then
-                Create('BoxHandleAdornment', Folder)
-                {
-                    Name = part.Name .. '_CHAM';
-                    Color3 = Settings['TCC'][2];
-                    AlwaysOnTop = true;
-                    Transparency = Settings['CT'];
-                    Visible = true;
-                    ZIndex = 10;
-                    
-                    Size = (part['Name'] == 'Head' and Vector3.new(1.25, 1.3, 1.25)) or (Vector3.new(.5, .5, .5) + part.Size)
-                }
+                local Target = Players:GetPlayerFromCharacter(char)
+				if (Target == Player) or (Target.TeamColor ~= Player.TeamColor) then return end
+				
+				local Folder = Chams['Team']:FindFirstChild(Target['Name'])
+					or Create('Folder', Chams['Team']){Name = Target['Name']}
+					
+				for __, part in next, (char:GetChildren()) do
+					if part:IsA('BasePart') then
+						Create('BoxHandleAdornment', Folder)
+						{
+							Name = part.Name .. '_CHAM';
+							Color3 = Settings['TCC'][2];
+							AlwaysOnTop = true;
+							Transparency = Settings['CT'];
+							Visible = true;
+							ZIndex = 10;
+							
+							Size = (part['Name'] == 'Head' and Vector3.new(1.25, 1.3, 1.25))
+								or (Vector3.new(.5, .5, .5) + part.Size)
+						}['Adornee'] = part
+					end
+				end
             end
         end
   	end    
