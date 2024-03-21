729e1b73380c4282adbbba558dd0219de1b5adcf
bubbapuff2011 <98616123+bubbapuff2011@users.noreply.github.com>
2024-03-21 18:41:30 +0000

    Add files via upload

--- roboblox.lua
+++ roboblox.lua
@@ -38,7 +38,7 @@ local Window = Rayfield:CreateWindow({
     CurrentValue = 16,
     Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+        game.Players.LocalPlayer.Character.Humanoid:SetAttribute("SpeedMultiplier", Value)
     end,
  })
 
