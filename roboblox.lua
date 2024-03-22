6fd47a455d33d04a32779a3fbd203d64418cd6b0
bubbapuff2011 <98616123+bubbapuff2011@users.noreply.github.com>
2024-03-22 18:00:49 +0000

    Add files via upload

--- roboblox.lua
+++ roboblox.lua
@@ -50,7 +50,7 @@ local Window = Rayfield:CreateWindow({
    CurrentValue = 180,
    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-       game.Players.LocalPlayer.VisionRadius = Value
+       game.Players.LocalPlayer:WaitForChild("VisionRadius") = Value
    end,
 })
 
