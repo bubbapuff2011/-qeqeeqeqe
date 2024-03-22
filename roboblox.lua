7ca30c1e78a4c7030f80d71b6e75b4ab812dda8f
bubbapuff2011 <98616123+bubbapuff2011@users.noreply.github.com>
2024-03-22 18:50:41 +0000

    Add files via upload

--- roboblox.lua
+++ roboblox.lua
@@ -3,54 +3,78 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Ocean Hub",
-    LoadingTitle = "Ocean Hub V1.1.1",
-    LoadingSubtitle = "by Ocean Project",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Big Hub"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = false, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
- })
+   Name = "Ocean Private Hub",
+   LoadingTitle = "Ocean Hub V1.1.1",
+   LoadingSubtitle = "by Ocean Project",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Ocean Gate",
+      Subtitle = "Key System",
+      Note = "You need a key directly supplied by me",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Ligma"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
- local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
+local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
+local VisualTab = Window:CreateTab("Visual", 4483362458) -- Title, Image
 
- local Slider = PlayerTab:CreateSlider({
-    Name = "Zoom Distance",
-    Range = {200, 100000},
-    Increment = 50,
-    Suffix = "Distance",
-    CurrentValue = 200,
-    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-        game.Players.LocalPlayer.CameraMaxZoomDistance = Value
-    end,
- })
-
- local Slider = PlayerTab:CreateSlider({
-   Name = "Instinct Radius",
-   Range = {180, 1000000090},
-   Increment = 10,
+local Slider = VisualTab:CreateSlider({
+   Name = "Zoom Distance",
+   Range = {200, 100000},
+   Increment = 50,
    Suffix = "Distance",
-   CurrentValue = 180,
-   Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   CurrentValue = 200,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-       game.Players.LocalPlayer:WaitForChild("VisionRadius") = Value
+      game.Players.LocalPlayer.CameraMaxZoomDistance = Value
    end,
 })
 
+local Slider = PlayerTab:CreateSlider({
+   Name = "WalkSpeed",
+   Range = {1, 10},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 10,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+    game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
+   end,
+})
+
+local Slider = PlayerTab:CreateSlider({
+   Name = "Dash length",
+   Range = {10, 1000},
+   Increment = 1,
+   Suffix = "Length",
+   CurrentValue = 10,
+   Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+    game.Players.LocalPlayer.Character:SetAttribute("DashLength", Value)
+   end,
+})
+
+local Slider = PlayerTab:CreateSlider({
+   Name = "Jump Height",
+   Range = {10, 500},
+   Increment = 1,
+   Suffix = "Height",
+   CurrentValue = 10,
+   Flag = "Slider3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+   end,
+})
